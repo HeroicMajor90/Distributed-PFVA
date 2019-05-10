f333d94dd783a63134649836236ab6ea4f220055
Martín Urtiz Gutiérrez <jmug1996@gmail.com>
2019-05-10 15:42:17 -0500

    Slicing function updated. Lines no longer than 80 chars

--- global_array.py
+++ global_array.py
@@ -33,6 +33,59 @@ class GlobalArray(object):
         return cumsum
 
 
+    def _key2slice(self, key):
+        assert (isinstance(key, slice) or isinstance(key, (int, np.integer)),
+                'Keys must be slices or integers')
+        
+        return key if isinstance(key, slice) else slice(key, key+1)
+
+
+    def _slice_array(self, slice_axis1, slice_axis2=None):
+        start, stop, step = slice_axis1.indices(self.total_rows)
+        slice_axis2 = slice_axis2 if slice_axis2 else slice(self.total_cols)
+        assert step > 0, 'Negative steps are not currently supported'
+
+        start = start + self.total_rows if start < 0 else max(start, 0)
+        stop = (stop + self.total_rows
+                if stop < 0 else
+                min(stop, self.total_rows))
+
+        s_amount = np.zeros(self.nodes).astype(int)
+        r_amount = np.zeros(self.nodes).astype(int)
+
+        new_total_rows = len(range(start, stop, step))
+        new_total_cols = len(range(*slice_axis2.indices(self.total_cols)))
+        rows_to_send = []
+
+        for new_idx, idx in enumerate(range(start, stop, step)):
+            s_node = self._row2nodeid(idx)
+            r_node = self._row2nodeid(new_idx, new_total_rows)
+
+            if s_node == self.node_id:
+                rows_to_send.append(self.local[idx - self.offset, slice_axis2])
+                s_amount[r_node] += 1
+
+            if r_node == self.node_id:
+                r_amount[s_node] += 1
+
+        sendbuf = np.asarray(
+            rows_to_send if rows_to_send else self.local, dtype=np.float64)
+
+        recvbuf = np.empty(
+            (max(r_amount.sum(), 1), new_total_cols), dtype=np.float64)
+
+        s_offsets = self._cumsum(s_amount)*new_total_cols
+        r_offsets = self._cumsum(r_amount)*new_total_cols
+        s_amount *= new_total_cols
+        r_amount *= new_total_cols
+
+        self.comm.Alltoallv(
+            [sendbuf, s_amount, s_offsets, MPI.DOUBLE],
+            [recvbuf, r_amount, r_offsets, MPI.DOUBLE])
+
+        return GlobalArray(new_total_rows, self.total_cols, local=recvbuf)
+
+
     def __init__(self, total_rows, total_cols=None, local=None):
         total_cols = total_rows if total_cols is None else total_cols
         self.total_rows = total_rows
@@ -89,8 +142,11 @@ class GlobalArray(object):
 
     def __add__(self, other):
         if isinstance(other, GlobalArray):
-            return GlobalArray(self.total_rows, self.total_cols, local=self.local + other.local)
-        return GlobalArray(self.total_rows, self.total_cols, local=self.local + other)
+            return GlobalArray(self.total_rows,
+                               self.total_cols,
+                               local=self.local + other.local)
+        return GlobalArray(
+            self.total_rows, self.total_cols, local=self.local + other)
 
 
     def __radd__(self, other):
@@ -99,18 +155,25 @@ class GlobalArray(object):
 
     def __sub__(self, other):
         if isinstance(other, GlobalArray):
-            return GlobalArray(self.total_rows, self.total_cols, local=self.local - other.local)
-        return GlobalArray(self.total_rows, self.total_cols, local=self.local - other)
+            return GlobalArray(self.total_rows,
+                               self.total_cols,
+                               local=self.local - other.local)
+        return GlobalArray(
+            self.total_rows, self.total_cols, local=self.local - other)
 
 
     def __rsub__(self, other):
-        return GlobalArray(self.total_rows, self.total_cols, local=other - self.local)
+        return GlobalArray(
+            self.total_rows, self.total_cols, local=other - self.local)
 
 
     def __mul__(self, other):
         if isinstance(other, GlobalArray):
-            return GlobalArray(self.total_rows, self.total_cols, local=self.local * other.local)
-        return GlobalArray(self.total_rows, self.total_cols, local=self.local * other)
+            return GlobalArray(self.total_rows,
+                               self.total_cols,
+                               local=self.local * other.local)
+        return GlobalArray(
+            self.total_rows, self.total_cols, local=self.local * other)
 
 
     def __rmul__(self, other):
@@ -119,71 +182,34 @@ class GlobalArray(object):
 
     def __div__(self, other):
         if isinstance(other, GlobalArray):
-            return GlobalArray(self.total_rows, self.total_cols, local=self.local / other.local)
-        return GlobalArray(self.total_rows, self.total_cols, local=self.local / other)
+            return GlobalArray(self.total_rows,
+                               self.total_cols,
+                               local=self.local / other.local)
+        return GlobalArray(
+            self.total_rows, self.total_cols, local=self.local / other)
 
 
     def __rdiv__(self, other):
-        return GlobalArray(self.total_rows, self.total_cols, local=other / self.local)
+        return GlobalArray(
+            self.total_rows, self.total_cols, local=other / self.local)
 
 
     def __getitem__(self, key):
         if isinstance(key, slice):
-            start, stop, step = key.indices(self.total_rows)
+            return self._slice_array(key)
 
-            assert step > 0, 'Negative steps are not currently supported'
-
-            start = start + self.total_rows if start < 0 else max(start, 0)
-            stop = stop + self.total_rows if stop < 0 else min(stop, self.total_rows)
-
-            s_amount = np.zeros(self.nodes).astype(int)
-            r_amount = np.zeros(self.nodes).astype(int)
-
-            new_total_rows = len(range(start, stop, step))
-            rows_to_send = []
-
-            for new_idx, idx in enumerate(range(start, stop, step)):
-                s_node = self._row2nodeid(idx)
-                r_node = self._row2nodeid(new_idx, new_total_rows)
-
-                if s_node == self.node_id:
-                    rows_to_send.append(self.local[idx - self.offset, :])
-                    s_amount[r_node] += 1
-
-                if r_node == self.node_id:
-                    r_amount[s_node] += 1
-
-            sendbuf = np.asarray(rows_to_send, dtype=np.float64) if len(rows_to_send) else self.local
-            recvbuf = np.empty((max(r_amount.sum(), 1), self.total_cols), dtype=np.float64)
-
-            s_offsets = self._cumsum(s_amount)*self.total_cols
-            r_offsets = self._cumsum(r_amount)*self.total_cols
-            s_amount *= self.total_cols
-            r_amount *= self.total_cols
-
-            self.comm.Alltoallv(
-                [sendbuf, s_amount, s_offsets, MPI.DOUBLE],
-                [recvbuf, r_amount, r_offsets, MPI.DOUBLE])
-
-            return GlobalArray(new_total_rows, self.total_cols, local=recvbuf)
-
-        elif isinstance(key, int):
-            id_with_local = self._row2nodeid(key)
-            if self.node_id == 0:
-                local = np.empty((1, self.total_cols))
-                self.comm.Recv([local, MPI.DOUBLE], source=id_with_local)
-            else:
-                local = None
-
-            if self.node_id == id_with_local:
-                self.comm.Send(self.local[key - self.row_offset, :], dest=0)
-
-            return GlobalArray(1, self.total_cols, local=local)
+        elif isinstance(key, (int, np.integer)):
+            return self._slice_array(self._key2slice(key))
 
         elif isinstance(key, tuple):
-            raise TypeError, 'Fancy slicing hasn"t been implemented'
+            return self._slice_array(
+                *(self._key2slice(axis_key) for axis_key in key))
         else:
             raise TypeError, 'Global Arrays indices must be integers or slice'
+
+
+    def __setitem__(self, ):
+        pass
 
 
     def __eq__(self, other):  # Total-wise
@@ -226,7 +252,8 @@ class GlobalArray(object):
         assert self.total_cols == other.total_rows
         res = GlobalArray(self.total_rows, other.total_cols)
         other_rows = self._get_rows_per_node(other.total_rows, other.nodes)
-        other_offsets = self._get_offsets_per_node(other.total_rows, other.nodes)
+        other_offsets = self._get_offsets_per_node(other.total_rows,
+                                                   other.nodes)
 
         current_col = np.empty(other.total_rows, np.float64)
         for col in range(other.total_cols):
@@ -250,8 +277,10 @@ class GlobalArray(object):
                 loc_y = y - low_bound
                 loc_x = x
                 return node, [loc_y, loc_x]
-        raise Exception("y value: " + str(y) +
-                        " out of bounds, higher than or eq to" + str(self.total_rows))
+        raise Exception("y value: "
+                        + str(y)
+                        + " out of bounds, higher than or eq to"
+                        + str(self.total_rows))
 
 
     def rref(self):
@@ -265,13 +294,15 @@ class GlobalArray(object):
                 current_column, current_column)
 
             ############# SET MAX PIVOT START ###############
-            if self.node_id < current_pivot_node or self.rows < 1:  # Node is irrelevant if above pivot
+            if self.node_id < current_pivot_node or self.rows < 1:
+                # Node is irrelevant if above pivot
                 senddata = np.array([0, self.node_id],dtype=np.float64)
             elif self.node_id == current_pivot_node:  # If node is pivot_node
                 a = np.abs(self.local[pivotCoords[0]:self.rows, current_column])
                 maxind = np.argmax(a) + pivotCoords[0]
                 senddata = np.array([np.amax(a), self.node_id])
-            elif self.node_id > current_pivot_node:  # If node is under pivot_node
+            elif self.node_id > current_pivot_node:
+                # If node is under pivot_node
                 a = np.abs(self.local[:self.rows, current_column])
                 maxind = np.argmax(a)
                 senddata = np.array([np.amax(a), self.node_id])
@@ -293,7 +324,8 @@ class GlobalArray(object):
                     sendrow = self.local[maxind, :]
 
                     self.comm.Sendrecv(
-                        sendrow, dest=current_pivot_node, recvbuf=mem, source=current_pivot_node)
+                        sendrow, dest=current_pivot_node, recvbuf=mem,
+                        source=current_pivot_node)
 
                     self.local[maxind, :] = mem
                 if self.node_id == current_pivot_node:  # If, pivot node
@@ -329,23 +361,29 @@ class GlobalArray(object):
             self.comm.Bcast(reduction_row, root=current_pivot_node)
 
             if self.node_id == current_pivot_node:
-                if pivotCoords[0] != self.rows:  # If there is local elimination to be done
-                    for local_row in range(pivotCoords[0] + 1, self.rows):  # Repeat for each local row under pivot
-                        c = self.local[local_row, current_column] / reduction_row[current_column]
+                # If there is local elimination to be done
+                if pivotCoords[0] != self.rows:
+                    # Repeat for each local row under pivot
+                    for local_row in range(pivotCoords[0] + 1, self.rows):
+                        c = (self.local[local_row, current_column]
+                             / reduction_row[current_column])
                         for column in range(current_column, self.total_cols):
-                            self.local[local_row, column] -= self.local[pivotCoords[0], column] * c
+                            self.local[local_row, column] -= (
+                                self.local[pivotCoords[0], column] * c)
 
             if self.node_id > current_pivot_node:  # In progress
                 for local_row in range(self.rows):
-                    c = self.local[local_row, current_column] / reduction_row[current_column]
+                    c = (self.local[local_row, current_column]
+                         / reduction_row[current_column])
                     for column in range(current_column, self.total_cols):
-                        self.local[local_row, column] -= reduction_row[column] * c
+                        self.local[local_row, column] -= (
+                            reduction_row[column] * c)
             self.comm.Barrier()
             ############# ROW REDUCTION END ###############
 
         ############# BACK SUBSTIUTION START ###############
-        for current_column in range(min(self.total_rows, self.total_cols) - 1, -1, -1):
-
+        for current_column in range(
+                min(self.total_rows, self.total_cols) - 1, -1, -1):
             current_pivot_node, pivotCoords = self._global_to_local(
                 current_column, current_column)
 
@@ -357,14 +395,20 @@ class GlobalArray(object):
             self.comm.Bcast(reduction_row, root=current_pivot_node)
 
             if self.node_id == current_pivot_node:
-                for row in range(pivotCoords[0]):  # Repeat for each local row over pivot
-                    c = self.local[row, current_column] / reduction_row[current_column]
+                # Repeat for each local row over pivot
+                for row in range(pivotCoords[0]):
+                    c = (self.local[row, current_column]
+                         / reduction_row[current_column])
                     for column in range(current_column, self.total_cols):
-                        self.local[row, column] -= self.local[pivotCoords[0], column] * c
-                self.local[pivotCoords[0], :] /= self.local[pivotCoords[0], pivotCoords[1]]
+                        self.local[row, column] -= (
+                            self.local[pivotCoords[0], column] * c)
+                self.local[pivotCoords[0], :] /= self.local[pivotCoords[0],
+                                                            pivotCoords[1]]
 
             if self.node_id < current_pivot_node:  # In progress
                 for local_row in range(self.rows):
-                    c = self.local[local_row, current_column] / reduction_row[current_column]
+                    c = (self.local[local_row, current_column]
+                         / reduction_row[current_column])
                     for column in range(current_column, self.total_cols):
-                        self.local[local_row, column] -= reduction_row[column] * c
+                        self.local[local_row, column] -= (
+                            reduction_row[column] * c)

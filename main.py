f02af09af915e9f59bfc23e9913ae9877230d781
Martín Urtiz Gutiérrez <jmug1996@gmail.com>
2019-05-10 15:42:36 -0500

    Test added for slicing

--- main.py
+++ main.py
@@ -16,6 +16,70 @@ for i in range(1000):
     stop = np.random.randint(start, max(start + 1, shape[0]), 1, np.int32)[0]
     step = np.random.randint(1, max(stop-start+1, 2), 1, np.int32)[0]
 
+    start1 = np.random.randint(0, shape[1], 1, np.int32)[0]
+    stop1 = np.random.randint(start, max(start1 + 1, shape[0]), 1, np.int32)[0]
+    step1 = np.random.randint(1, max(stop1-start1+1, 2), 1, np.int32)[0]
+
+    start = MPI.COMM_WORLD.bcast(start, root=0)
+    stop = MPI.COMM_WORLD.bcast(stop, root=0)
+    step = MPI.COMM_WORLD.bcast(step, root=0)
+
+    start1 = MPI.COMM_WORLD.bcast(start1, root=0)
+    stop1 = MPI.COMM_WORLD.bcast(stop1, root=0)
+    step1 = MPI.COMM_WORLD.bcast(step1, root=0)
+
+    AS = A[start:stop:step, start1:stop1:step1]
+
+    AS_ga = GlobalArray.array(A[start:stop:step, start1:stop1:step1])
+
+    Sliced_Array = A_ga[start:stop:step, start1:stop1:step1]
+
+    if Sliced_Array != AS_ga:
+        AS_ga.disp()
+        Sliced_Array.disp()
+        raise Exception("FAIL")
+    elif MPI.COMM_WORLD.Get_rank() == 0:
+        print(i)
+
+
+print("TEST: Matrix indexing")
+shape = np.empty(2, dtype=np.int32)
+for i in range(1000):
+    shape[:] = np.random.randint(1, 1000, 2, np.int32)
+    MPI.COMM_WORLD.Bcast(shape)
+    A = 1000 * np.random.rand(shape[0], shape[1])
+    MPI.COMM_WORLD.Bcast(A)
+    A_ga = GlobalArray.array(A)
+
+    index = np.random.randint(0, shape[0], 1, np.int32)[0]
+
+    index = MPI.COMM_WORLD.bcast(index, root=0)
+
+    AS_ga = GlobalArray.array(A[index][np.newaxis])
+
+    Indexed_Array = A_ga[index]
+
+    if Indexed_Array != AS_ga:
+        AS_ga.disp()
+        Indexed_Array.disp()
+        raise Exception("FAIL")
+    elif MPI.COMM_WORLD.Get_rank() == 0:
+        print(i)
+
+
+print("TEST: Matrix row slicing")
+shape = np.empty(2, dtype=np.int32)
+for i in range(1000):
+    shape[:] = np.random.randint(1, 1000, 2, np.int32)
+    MPI.COMM_WORLD.Bcast(shape)
+    A = 1000 * np.random.rand(shape[0], shape[1])
+    MPI.COMM_WORLD.Bcast(A)
+    A_ga = GlobalArray.array(A)
+
+    start = np.random.randint(0, shape[0], 1, np.int32)[0]
+    stop = np.random.randint(start, max(start + 1, shape[0]), 1, np.int32)[0]
+    step = np.random.randint(1, max(stop-start+1, 2), 1, np.int32)[0]
+
     start = MPI.COMM_WORLD.bcast(start, root=0)
     stop = MPI.COMM_WORLD.bcast(stop, root=0)
     step = MPI.COMM_WORLD.bcast(step, root=0)
@@ -30,7 +94,7 @@ for i in range(1000):
         raise Exception("FAIL")
     elif MPI.COMM_WORLD.Get_rank() == 0:
         print(i)
-    MPI.COMM_WORLD.Barrier()
+
 
 print("TEST: Matrix Transpose")
 shape = np.empty(2, dtype=np.int32)

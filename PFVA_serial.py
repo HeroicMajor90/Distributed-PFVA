3001a5953e809f2d181895be5607b83708ee0f1a
aureliopuebla <aureliopuebla_9@hotmail.com>
2019-05-11 20:19:47 -0500

    Adds serial version up to plot of F vs prob dist

--- PFVA_serial.py
+++ PFVA_serial.py
@@ -1,30 +1,100 @@
 #!/usr/bin/env python
 import numpy as np
+import sys
+import matplotlib.pyplot as plt
+
+PROBABILITY_WINDOW_SIZE = 21  # Must be odd
+F_INDEX = 0
+
+CATEGORIES = [
+    "Outdoors-n-Adventures",
+    "Tech",
+    "Family",
+    "Health-n-Wellness",
+    "Sports-n-Fitness",
+    "Learning",
+    "Photography",
+    "Food-n-Drink",
+    "Writing",
+    "Language-n-Culture",
+    "Music",
+    "Movements",
+    "LGBTQ",
+    "Film",
+    "Sci-Fi-n-Games",
+    "Beliefs",
+    "Arts",
+    "Book Clubs",
+    "Dance",
+    "Hobbies-n-Crafts",
+    "Fashion-n-Beauty",
+    "Social",
+    "Career-n-Business",
+    "Gardening-n-Outdoor housework",
+    "Cooking",
+    "Theatre, Show, Performance, Concerts",
+    "Drinking alcohol, Partying",
+    "Sex and Making Love",
+]
+
+
+def normalize(X):
+    return (X - np.mean(X, axis=0)) / (np.sqrt(X.shape[0]) * np.std(X, axis=0))
 
 
 def PCA(X):
-	print "Starting PCA"
-	rows, cols = X.shape
-	X = (X - np.mean(X, axis=0)) / (np.sqrt(rows)*np.std(X, axis=0))
-	sigma = X.T.dot(X)
-	S = np.identity(sigma.shape[0])
+    sigma = X.T.dot(X)
+    S = np.identity(sigma.shape[0])
+    for _ in xrange(40):
+        Q, R = np.linalg.qr(sigma, mode="complete")
+        sigma = R.dot(Q)
+        S = S.dot(Q)
+    return np.diagonal(sigma), S
 
-	for _ in xrange(40):
-		print _
-		Q, R = np.linalg.qr(sigma, mode="complete")
-		sigma = R.dot(Q)
-		S = S.dot(Q)
 
-	eigen_values = np.diagonal(sigma)
-	eigen_vectors = S
-	descending = cols - 1 - np.argsort(eigen_values)
+def sort_by_eigen_value(e_val, e_vect):
+    descending = np.argsort(-e_val)
+    return e_val[descending], e_vect[:, descending]
 
-	return eigen_values[descending], eigen_vectors[:, descending]
 
-	
+def sort_by_first_column(A):
+    ascending = np.argsort(A[:, 0])
+    return A[ascending, :]
+
+
+def get_probability_distribution(data, window_size):
+    offset = int(window_size / 2)
+    prob_dist = np.empty(np.size(data) - 2 * offset)
+    for i in range(offset, np.size(data) - offset):
+        prob_dist[i - offset] = (
+            np.sum(data[i - offset:i - offset + window_size]) / window_size)
+    return prob_dist
+
+
 def main():
-	D, Q = PCA(np.random.randint(0, 256, (300, 300), dtype=np.uint8))
-	print
+    X = np.loadtxt("dataM.txt")
+    X = normalize(X)
+    e_val, e_vect = PCA(X)
+    e_val, e_vect = sort_by_eigen_value(e_val, e_vect)
+    F = X.dot(e_vect)
+
+    cat_to_use = sys.argv[1] if len(sys.argv) > 1 else "Tech"
+    cat_index = CATEGORIES.index(cat_to_use) if cat_to_use in CATEGORIES else 0
+    cat_data = np.loadtxt("likesM.txt")[:, cat_index]
+
+    f_cat_data = np.stack([F[:, F_INDEX], cat_data], axis=-1)
+    f_cat_data = sort_by_first_column(f_cat_data)
+
+    prob_dist = get_probability_distribution(f_cat_data[:, 1],
+                                             PROBABILITY_WINDOW_SIZE)
+
+    offset = int(PROBABILITY_WINDOW_SIZE / 2)
+    plt.plot(f_cat_data[offset:f_cat_data.shape[0] - offset, 0], prob_dist)
+    plt.title("Feature%i vs Likes(%s)" % (F_INDEX, cat_to_use))
+    plt.xlabel("F%d" % F_INDEX)
+    plt.ylabel("p(f)")
+    plt.show()
+
 
 if __name__ == "__main__":
-	main()
+    main()
